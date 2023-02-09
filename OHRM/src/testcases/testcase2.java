b47baa9bfb171933d6c4584c4450bbdd590dd32c
satya <satya@Sasikala.attlocal.net>
2023-02-09 12:53:25 -0500

    made some changes in testcase2

--- OHRM/src/testcases/testcase2.java
+++ OHRM/src/testcases/testcase2.java
@@ -4,7 +4,7 @@ public class testcase2 {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("This is testcase2");
+		System.out.println("This is testcase2 after some changes ");
 
 	}
 
